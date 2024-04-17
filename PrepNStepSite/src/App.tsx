54194c8820ca58933cf9f146109f4fac918914f5
connorjohnson6 <connor.johnson1@marist.edu>
2024-04-17 12:24:09 -0400

    Checking for correctness

--- PrepNStepSite/src/App.tsx
+++ PrepNStepSite/src/App.tsx
@@ -1,12 +1,10 @@
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import AppRouter from "./components/router/Router";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen ">
       <div className="flex-grow">
-        <Header />
         <AppRouter />
       </div>
       <Footer />
--- PrepNStepSite/src/components/router/Router.tsx
+++ PrepNStepSite/src/components/router/Router.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../Header/Header";
 import Home from '../../pages/Home/Home';
 import WeeklyMenu from '../../pages/WeeklyMenu/WeeklyMenu';
 import MealPlans from '../../pages/MealPlans/MealPlans';
@@ -8,11 +9,14 @@ import Blog from '../../pages/Blog/Blog';
 import MuscleGain from '../../pages/MuscleGain/MuscleGain';
 import Cutting from '../../pages/Cutting/Cutting';
 import ChefsChoice from '../../pages/ChefsChoice/ChefsChoice';
-import NotFoundPage from '../../pages/404/404'; 
+import NotFoundPage from '../../pages/404/404';
+import ScrollToTop from '../ScrollToTop/ScrollToTop'; // Adjust the import path as necessary
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/weeklymenu" element={<WeeklyMenu />} />
@@ -22,7 +26,6 @@ const AppRouter: React.FC = () => {
         <Route path="/musclegain" element={<MuscleGain />} />
         <Route path="/cutting" element={<Cutting />} />
         <Route path="/chefschoice" element={<ChefsChoice />} />
-
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
